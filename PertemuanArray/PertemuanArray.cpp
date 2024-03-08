5b1389c7b4268659daa5188eb3f9f153961a48c0
CaptainAAI <anandaagungismail@gmail.com>
2024-03-08 11:00:25 +0700

    input data pada Array nya

--- PertemuanArray/PertemuanArray.cpp
+++ PertemuanArray/PertemuanArray.cpp
@@ -14,5 +14,13 @@ void input() {
 			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
 		}
 	}
-}
+	cout << endl;
+	cout << "==================================" << endl;
+	cout << "Masukkan Elemen Array" << endl;
+	cout << "==================================" << endl;
 
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> a[i];
+	}
+}

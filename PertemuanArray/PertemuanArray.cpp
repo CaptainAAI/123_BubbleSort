3fe5b6e04e017b039d37a9a31361ecc276819043
CaptainAAI <anandaagungismail@gmail.com>
2024-03-08 10:53:40 +0700

    Prosedur untuk input data Array dengan maksimal 20 Array

--- PertemuanArray/PertemuanArray.cpp
+++ PertemuanArray/PertemuanArray.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int a[20];
-int n;                
+int n;  
+
+void input() {
+	while (true) {
+		cout << "Masukkan banyaknya elemen pada array :    ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+		}
+	}
+}
 
